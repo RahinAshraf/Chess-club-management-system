63d718abd991ed1c0550ecf93960ac7899704332
Rahin Ashraf <rahin.ashraf@kcl.ac.uk>
2021-11-14 14:20:00 +0000

    Add placeholder template

--- clubs/views.py
+++ clubs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, response
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+    #response = HttpResponse("Welcome")
+    #return response
+
+
