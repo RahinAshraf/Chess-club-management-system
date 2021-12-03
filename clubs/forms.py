280b638a74ea79dc53e862941c89b3661c24c0cb
Shmeelok Chakraborty <shmeelok.chakraborty@kcl.ac.uk>
2021-12-03 00:59:53 +0000

    Modify View Functions to CBV
    - Modify Home view to include HomeView class
    - Modify Test view to include TestView class
    - Add Loginprohibited mixin
    - Add view class for creation of clubs and tournaments
    - Add view class for SignUp
    - Add view class for Profile Updation
    - Add helper methods in form to process the data sent to the form.

--- clubs/forms.py
+++ clubs/forms.py
@@ -3,6 +3,8 @@ from django import forms
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import check_password
+from django.contrib.auth import login
 from django.db.models import fields
 from django.forms import widgets
 from .models import Tournament, User
@@ -98,6 +100,20 @@ class PasswordForm(forms.Form):
     )
     password_confirmation = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
 
+    def process_valid_data(self,request):
+        if self.is_valid():
+            password = self.cleaned_data.get('password')
+            if check_password(password, request.user.password):
+                new_password = self.cleaned_data.get('new_password')
+                request.user.set_password(new_password)
+                request.user.save()
+                login(request, request.user)
+                return True
+            else:
+                return False
+        else:
+            return False
+
 
 class CreateNewClubForm(forms.ModelForm):
 
@@ -105,6 +121,18 @@ class CreateNewClubForm(forms.ModelForm):
         #Form options
         model = Club
         fields = ['name','location','mission_statement']
+
+    def process_valid_form(self,user):
+        """This method processes valid form data. It creates the necessary models and relationships
+           when valid data is sent."""
+        if self.is_valid():
+            club = self.save(commit=False)
+            club.club_owner = user
+            club.save()
+            # Create the new membership type. 
+            # The membership is being manually created because the club models' save method is called instead of create 
+            # which does not automatically create the new membership.
+            MembershipType.objects.create(user = user, club = club, type = consts.CLUB_OWNER)
 
 class CreateNewTournamentForm(forms.ModelForm):
 
@@ -117,4 +145,16 @@ class CreateNewTournamentForm(forms.ModelForm):
                                 error_messages={'required':'Please enter a capacity',
                                 'max_value':'The max value is 96','min_value':'The min value is 2'})
         
-    
+    def process_form_with_organiser_data(self,current_user_club,organising_officer):
+        """This method takes in the current club of the organiser and the organiser
+           to create a valid instance of Tournament object. If successfully created, it will
+           return the result of save being called on Tournament."""
+        if self.is_valid():
+            Tournament = self.save(commit=False)
+            Tournament.organising_officer = organising_officer
+            Tournament.club = current_user_club
+            capacity = self.cleaned_data.get('capacity')
+            Tournament.capacity = capacity
+            return Tournament.save()
+        else:
+            return None
--- clubs/views.py
+++ clubs/views.py
@@ -1,61 +1,69 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, response
 from .forms import CreateNewTournamentForm, SignUpForm
-from django.contrib.auth import authenticate, login, logout
-from .helpers import login_prohibited
+from django.contrib.auth import login, logout
+from django.views.generic.edit import UpdateView
 from .forms import LogInForm, UserForm, PasswordForm, CreateNewClubForm
 from django.contrib import messages
-from django.utils.decorators import method_decorator
 from django.views import View
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.hashers import check_password
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 from .models import Tournament, User, MembershipType, Club
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
+from django.views.generic.edit import FormView
+from django.urls import reverse
 from .Constants import consts
 from .Utilities import promote_demote_helper
 def get_club_choice(request):
     """Utility function to return the club name the user has selected."""
     return request.session['club_choice']
 
+class TestView(LoginRequiredMixin,View):
+    http_method_names = ['get']
 
+    def get(self, request):
+       self.club = None
+       self.membership_type = None
+       try:
+           self.club = request.session['club_choice']
+           self.membership_type = request.user.get_membership_type_in_club(self.club)
+       except:
+            render(request, 'test.html', {'club':None, 'membership_type':None})
+       return render(request, 'test.html', {'club':self.club, 'membership_type':self.membership_type})
 
-def home(request):
-    if request.user.is_authenticated:
-        return redirect('test')
-    else :
+class LoginProhibitedMixin:
+    """Mixin that redirects when a user is logged in."""
+    redirect_when_logged_in_url = None
+
+    def dispatch(self, *args, **kwargs):
+        """Redirect when logged in, or dispatch as normal otherwise."""
+        if self.request.user.is_authenticated:
+            url = self.get_redirect_when_logged_in_url()
+            return redirect(url)
+        return super().dispatch(*args, **kwargs)
+
+    def get_redirect_when_logged_in_url(self):
+        """Returns the url to be redirected to"""
+        if self.redirect_when_logged_in_url == None:
+            raise ImproperlyConfigured()
+        else:
+            return self.redirect_when_logged_in_url
+
+class HomeView(LoginProhibitedMixin,View):
+    http_method_names = ['get']
+
+    redirect_when_logged_in_url = 'test'
+
+    def get(self, request):
         return render(request, 'home.html')
-
-def test(request):
-    club = None
-    membership_type = None
-    try:
-        club = request.session['club_choice']
-        membership_type = request.user.get_membership_type_in_club(request.session['club_choice'])
-    except:
-        render(request, 'test.html', {'club':None, 'membership_type':None})
-
-    return render(request, 'test.html', {'club':club, 'membership_type':membership_type})
-
-def sign_up(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request,user)
-            return redirect('test')
-    else:
-        form = SignUpForm()
-    return render(request, 'sign_up.html', {'form': form})
-
-class LogInView(View):
+    
+class LogInView(LoginProhibitedMixin,View):
     """ View that handles Log in. """
 
     http_method_names = ['get', 'post']
 
-    @method_decorator(login_prohibited)
-    def dispatch(self, request):
-        return super().dispatch(request)
+    redirect_when_logged_in_url = 'test'
 
     def get(self, request):
         """ Display Log In template. """
@@ -77,6 +85,21 @@ class LogInView(View):
         """ Render Log in template with blank log in form."""
         form = LogInForm()
         return render(self.request, 'log_in.html', {'form': form, 'next': self.next})
+
+class SignUpView(LoginProhibitedMixin, FormView):
+    """View that signs up user."""
+
+    form_class = SignUpForm
+    template_name = "sign_up.html"
+    redirect_when_logged_in_url = 'test'
+
+    def form_valid(self, form):
+        self.object = form.save()
+        login(self.request, self.object)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('test')
         
 def log_out(request):
     logout(request)
@@ -103,18 +126,22 @@ def switch_club(request):
     else:
         return redirect('test')
 
-@login_required
-def profile(request):
-    current_user = request.user
-    if request.method == 'POST':
-        form = UserForm(instance=current_user, data=request.POST)
-        if form.is_valid():
-            messages.add_message(request, messages.SUCCESS, "Profile updated!")
-            form.save()
-            return redirect('test')
-    else:
-        form = UserForm(instance=current_user)
-    return render(request, 'profile.html', {'form': form})
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    """View to update logged-in user's profile."""
+
+    model = UserForm
+    template_name = "profile.html"
+    form_class = UserForm
+
+    def get_object(self):
+        """Return the object (user) to be updated."""
+        user = self.request.user
+        return user
+
+    def get_success_url(self):
+        """Return redirect URL after successful update."""
+        messages.add_message(self.request, messages.SUCCESS, "Profile updated!")
+        return reverse('test')
 
 def get_promote_and_demote_subject_user_with_club(request, user_id):
     """This function returns a dictionary of the subject user along with
@@ -212,19 +239,12 @@ def transfer_ownership(request, user_id):
 
 @login_required
 def password(request):
-    current_user = request.user
     if request.method == 'POST':
         form = PasswordForm(data=request.POST)
         if form.is_valid():
-            password = form.cleaned_data.get('password')
-            if check_password(password, current_user.password):
-                new_password = form.cleaned_data.get('new_password')
-                current_user.set_password(new_password)
-                current_user.save()
-                login(request, current_user)
-                messages.add_message(request, messages.SUCCESS, "Password updated!")
-                return redirect('test')
-        messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
+            form.process_valid_data(request=request)
+        else:
+            messages.add_message(request, messages.ERROR, "The credentials provided were invalid!")
     form = PasswordForm()
     return render(request, 'password.html', {'form': form})
 
@@ -241,7 +261,6 @@ def user_list(request):
         type = current_user.get_membership_type_in_club(current_user_club_name)
         return render(request, 'user_list.html', {'users': users, "type": type})
 
-
 @login_required
 def club_list(request):
     clubs = Club.objects.all()
@@ -253,33 +272,30 @@ def club_list(request):
 @login_required
 def apply_to_club(request, club_name):
     user_club_choice = Club.objects.get(pk = club_name)
-    print(user_club_choice)
-    print(request.user)
     # Make user an applicant of this club
     MembershipType.objects.create(user = request.user, club = user_club_choice, type = consts.APPLICANT)
     succes_string = 'You have successfully applied to' + club_name
     messages.add_message(request, messages.SUCCESS, succes_string)
     return club_list(request = request)
 
+class CreateNewClubView(LoginRequiredMixin,CreateView):
+    model = Club
+    template_name = 'create_club.html'
+    form_class = CreateNewClubForm
+    http_method_names = ['post', 'get']
 
-@login_required
-def create_new_club(request):
-    if request.method == 'POST':
-        form = CreateNewClubForm(request.POST)
-        if form.is_valid():
-            club = form.save(commit=False)
-            club.club_owner = request.user
-            club.save()
-            form.save()
-            # Create the new membership type. 
-            # The membership is being manually created because the club models' save method is called instead of create 
-            # which does not automatically create the new membership.
-            MembershipType.objects.create(user = request.user, club = club, type = consts.CLUB_OWNER)
-            messages.add_message(request, messages.SUCCESS, "You have created a new club!")
-            return redirect('club_list')
-    else:
-        form = CreateNewClubForm()
-    return render(request, 'create_club.html', {'form': form})
+    def form_valid(self, form):
+        """Process a valid form."""
+        form.process_valid_form(user = self.request.user)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Return URL to redirect the user too after valid form handling."""
+        return reverse('club_list')
+
+    def handle_no_permission(self):
+        return redirect('test')
 
 @login_required
 def show_tournaments(request):
@@ -295,31 +311,31 @@ def show_tournaments(request):
         type = current_user.get_membership_type_in_club(current_user_club_name)
         return render(request, 'tournament_list.html', {'tournaments': tournaments, "type": type,})
 
-@login_required
-def create_new_tournament(request):
-    current_user_club = Club.objects.get(pk = request.session['club_choice'])
-    if request.method == 'POST':
-        form = CreateNewTournamentForm(request.POST)
-        if form.is_valid():
-            Tournament = form.save(commit=False)
-            Tournament.organising_officer = request.user
-            Tournament.club = current_user_club
-            capacity = form.cleaned_data.get('capacity')
-            Tournament.capacity = capacity
-            Tournament.save()
-            form.save()
-            return redirect('tournaments')
-        
-    else:
-        form = CreateNewTournamentForm()
-    return render(request, 'create_tournament.html', {'form':form})
+class CreateNewTournamentView(LoginRequiredMixin,CreateView):
+    model = Tournament
+    template_name = 'create_tournament.html'
+    form_class = CreateNewTournamentForm
+    http_method_names = ['post', 'get']
 
+    def form_valid(self, form):
+        """Process a valid form."""
+        organising_officer = self.request.user
+        current_user_club = Club.objects.get(pk = self.request.session['club_choice'])
+        form.process_form_with_organiser_data(current_user_club = current_user_club, organising_officer = organising_officer)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        """Return URL to redirect the user too after valid form handling."""
+        return reverse('tournaments')
+
+    def handle_no_permission(self):
+        return redirect('test')
 
 @login_required
 def participate_in_tournament(request,tournament_id):
     current_user = request.user
     tournament = Tournament.objects.get(id = tournament_id)
-    print(tournament)
     tournament.participating_players.add(User.objects.get(email = request.user.email))
     tournament_name = tournament.name
     messages.add_message(request, messages.SUCCESS, "You have successfully joined the tournament: " + tournament_name)
