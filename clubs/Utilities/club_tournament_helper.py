63752a43234cf4084a8297dd9ef8fae367610c20
Shmeelok Chakraborty <shmeelok.chakraborty@kcl.ac.uk>
2021-12-15 23:26:56 +0000

    Add doc string and comments

--- clubs/Utilities/club_tournament_helper.py
+++ clubs/Utilities/club_tournament_helper.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from ..models import Tournament
 
 def does_club_have_some_tournament(club):
+    """This checks if club has some tournament. If not then it raises ObjectDoesNotExist exception."""
     if Tournament.objects.filter(club=club).count() == 0:
         raise ObjectDoesNotExist('Club does not have any tournament.')
     return True
