78401186efdec828b682a4583c1ad71401040061
Adnan Salah <adnan.salah@kcl.ac.uk>
2021-12-17 01:23:55 +0000

    Manually remove default id from 007 migration

--- clubs/migrations/0007_auto_20211125_1309.py
+++ clubs/migrations/0007_auto_20211125_1309.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='membershiptype',
             name='id',
-            field=models.AutoField(default=1, primary_key=True, serialize=False),
+            field=models.AutoField(primary_key=True, serialize=False),
         ),
         migrations.AlterField(
             model_name='membershiptype',
